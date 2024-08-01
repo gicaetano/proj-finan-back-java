f2c8ad8b8c008b26781fa720cd29d6caeda2b4a3
Gisele <gisele_sig@hotmail.com>
2024-08-01 12:30:09 -0300

    Incluido Spring Security e criptografica de senha login

--- controlli/src/main/java/br/com/xlcode/controlli/service/impl/UsuarioServiceImpl.java
+++ controlli/src/main/java/br/com/xlcode/controlli/service/impl/UsuarioServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.xlcode.controlli.service.impl;
 
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,47 +16,40 @@ import br.com.xlcode.controlli.service.UsuarioService;
 public class UsuarioServiceImpl implements UsuarioService {
 	
 	private UsuarioRepository repository;
+	private PasswordEncoder encoder;
 
-	public UsuarioServiceImpl(UsuarioRepository repository) {
+	public UsuarioServiceImpl(UsuarioRepository repository, PasswordEncoder encoder) {
 		super();
 		this.repository = repository;
+		this.encoder = encoder;
 	}
 
 	public Usuario autenticar(String email, String senha) {
-		Optional<Usuario> usuarioOptional = repository.findByEmail(email);
+		Optional<Usuario> usuario = repository.findByEmail(email);
 
-		if (!usuarioOptional.isPresent()) {
+		if (usuario.isEmpty()) {
 			throw new ErroAutenticacao("Usuário não encontrado para o e-mail informado.");
 		}
 
-		Usuario usuario = usuarioOptional.get();
+		boolean senhasBatem = encoder.matches(senha, usuario.get().getSenha());
 
-		// Comparação simples de senha
-		if (!usuario.getSenha().equals(senha)) {
+		if (!senhasBatem) {
 			throw new ErroAutenticacao("Senha inválida");
 		}
-
-		return usuario;
+		return usuario.get();
 	}
-
-//	public Usuario autenticar(String email, String senha) {
-//		Optional<Usuario> usuario = repository.findByEmail(email);
-//
-//		if(!usuario.isPresent()) {
-//			throw new ErroAutenticacao("Usuário não encontrado, para o e-mail informado.");
-//		}
-//
-//		if(usuario.get().getSenha().equals(senha)) {
-//			throw new ErroAutenticacao("Senha inválida");
-//		}
-//
-//		return null;
-//	}
 
 	@Transactional
 	public Usuario salvarUsuario(Usuario usuario) {
 		validarEmail(usuario.getEmail());
+		criptografarSenha(usuario);
 		return repository.save(usuario);
+	}
+
+	private void criptografarSenha(Usuario usuario) {
+		String senha = usuario.getSenha();
+		String senhaCripto = encoder.encode(senha);
+		usuario.setSenha(senhaCripto);
 	}
 
 	public void validarEmail(String email) {
