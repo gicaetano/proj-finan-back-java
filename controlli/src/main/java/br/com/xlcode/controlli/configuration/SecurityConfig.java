f4eed872b2d0b11a04afe67d94cf711887d9888a
Gisele <gisele_sig@hotmail.com>
2024-08-01 13:00:28 -0300

    Conf permissao API

--- controlli/src/main/java/br/com/xlcode/controlli/configuration/SecurityConfig.java
+++ controlli/src/main/java/br/com/xlcode/controlli/configuration/SecurityConfig.java
@@ -1,10 +1,12 @@
 package br.com.xlcode.controlli.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,20 +21,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        String SenhaCodificada = passwordEncoder().encode("qwe123");
-
+        String senhaCodificada = passwordEncoder().encode("qwe123");
         auth
                 .inMemoryAuthentication()
                 .withUser("usuario")
-                .password(SenhaCodificada)
+                .password(senhaCodificada)
                 .roles("USER");
     }
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST,"/api/usuarios/autenticar").permitAll()
+                    .antMatchers(HttpMethod.POST,"/api/usuarios").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                     .httpBasic();
     }
